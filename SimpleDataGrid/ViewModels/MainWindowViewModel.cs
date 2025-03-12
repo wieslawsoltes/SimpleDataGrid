75caeb67641ad53f2315b7152b2c37a965992dc4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-03-12 17:59:20 +0100

    Update MainWindowViewModel.cs

--- SimpleDataGrid/ViewModels/MainWindowViewModel.cs
+++ SimpleDataGrid/ViewModels/MainWindowViewModel.cs
@@ -10,22 +10,26 @@ public class ItemViewModel : ViewModelBase
     public string Item1 { get; set; }
     public string Item2 { get; set; }
     public string Item3 { get; set; }
+    public double Height { get; set; }
 }
 
 public class MainWindowViewModel : ViewModelBase
 {
     public MainWindowViewModel()
     {
-        // var rand = new Random();
+        var rand = new Random();
         Items = new ObservableCollection<ItemViewModel>(
             Enumerable
-                .Range(0, 1_000_000)
+                //.Range(0, 1_000_000)
+                .Range(0, 100_000)
                 .Select(x => new ItemViewModel()
                 {
                     Item0 = $"Item0_{x}",
                     Item1 = $"Item1_{x}",
                     Item2 = $"Item2_{x}",
                     Item3 = $"Item3_{x}",
+                    Height = double.NaN,
+                    // Height = rand.Next(20, 60),
                     // Item3 = string.Join(Environment.NewLine, Enumerable.Repeat($"Item3_{x}", rand.Next(1, 10))),
                 }));
     }
